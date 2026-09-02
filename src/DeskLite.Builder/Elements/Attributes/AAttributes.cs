460065fe0c3589404cc7966649bfc07ad33367fe
jansthcirlu <jan.ulrichts@gmail.com>
2026-09-03 00:18:02 +0200

    Added a, div, p.

---
+++ src/DeskLite.Builder/Elements/Attributes/AAttributes.cs
@@ -0,0 +1,10 @@
+using DeskLite.Builder.Elements.Attributes.Base;
+
+namespace DeskLite.Builder.Elements.Attributes;
+
+public sealed record AAttributes : ElementAttributes<A>
+{
+    public string? Download { get; set; }
+    public string? Href { get; set; }
+    public string? Type { get; set; }
+}
